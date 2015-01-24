d881f164e961f55458f6e6ba1a5e2071e370d119
Jakub Macek <1@jakubmacek.cz>
2015-01-24 15:14:54 +0100

    Cleaned up initial implementation.

---
+++ DownloadHostedImages/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using WatiN.Core;
+
+namespace DownloadHostedImages
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var wc = new WebClient();
+
+            using (var browser = new IE())
+            {
+                browser.GoTo(args[1]);
+
+                var links = browser.Links.Where(x => x.ClassName == "external");
+                var urls = links.Where(link => link.ChildrenWithTag("img").Any()).Select(link => link.Url).Where(url => url.Contains(".jpg")).ToList();
+                foreach (var url in urls)
+                {
+                    var pageUri = new Uri(url);
+                    var fileName = pageUri.Segments.Last();
+                    if (!File.Exists(fileName))
+                    {
+                        browser.GoTo(url);
+                        var imageSrc = browser.Image("show_image").Src;
+                        var imageUri = new Uri(imageSrc);
+                        wc.DownloadFile(imageUri, fileName);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+DownloadHostedImages is a minimalistic tool for downloading full size images
+from pixhost.org given a URL as a command line argument. It's designed for a
+specific website and I don't particularly care to extend it, but pull requests
+are welcome.
